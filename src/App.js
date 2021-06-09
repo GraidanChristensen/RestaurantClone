5564604245cf09c0de28831fb30342f57c57c277
GraidanChristensen <graidanc@gmail.com>
2021-06-08 21:26:30 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <main>
+        <Header/>
+        {routes}
+      </main>
       <footer>
         <Footer/>
       </footer>
